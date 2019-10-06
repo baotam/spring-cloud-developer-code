98098588a18f14c12b5d66f7f8f96359b72c2c85
Baotam Nguyen <baotam_nguyen@intuit.com>
2019-10-06 14:49:10 -0500

    enable client side load balancing and service discovery

--- components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
+++ components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
@@ -1,5 +1,6 @@
 package io.pivotal.pal.tracker.restsupport;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestOperations;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 public class RestConfig {
 
     @Bean
+    @LoadBalanced
     public RestOperations restOperations() {
         return new RestTemplate();
     }
